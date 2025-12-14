98e51e35d4bec90c185078df08afe5839cdb03e4
Fae <109899113+iamf-a-e@users.noreply.github.com>
2025-12-14 18:32:14 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 import os 
 import logging
 from datetime import datetime
-import instructions, faq_data
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
