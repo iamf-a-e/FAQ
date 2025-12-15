f558f980d58c236f32752077076176f2e2ba6fdd
Fae <109899113+iamf-a-e@users.noreply.github.com>
2025-12-15 17:18:25 +1000

    Update instructions.py

--- api/instructions.py
+++ api/instructions.py
@@ -1,4 +1,4 @@
-from training import products
+import products
 
 company_name="Umbrella Labs"
 company_address="Level 4/80 Ann St, Brisbane City QLD"
@@ -17,7 +17,7 @@ instructions = (
     f"So introduce yourself as {company_name}'s online assistant.\n\n"
     
     "**Behavior:**\n\n"
-    "- Always maintain a professional and courteous tone, be friendly.\n"
+    "- Always maintain a professional and courteous tone.\n"
     "- Respond to queries with clear and concise information.\n"
     "- If a conversation topic is out of scope, inform the customer and guide them back to the company-related topic. If the customer repeats this behavior, stop the chat with a proper warning message.\n"
     "  This must be strictly followed\n\n"
